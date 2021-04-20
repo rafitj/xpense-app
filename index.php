4dbe17e7e52978a483562819fdd7c28592ccd806
Rafit Jamil <rafit.jamil@gmail.com>
2021-04-20 05:19:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Rafit's Expensify Challenge</title>
+    <title>Xpense App</title>
     <!-- Allow Media Query for Mobile -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Stylesheets -->
